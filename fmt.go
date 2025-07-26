54c1d0106d7d77431911a5def9d8f180209330b7
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-07-26 23:39:18 +0700

    feat : library fmt

---
+++ fmt.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main(){
+	firstName := "Fajar"
+	lastName := "Fadhillah"
+
+	fmt.Println("Hello, '", firstName, lastName, "'")
+	fmt.Printf("Hello '%s %s'\n", firstName, lastName)
+
+
+}
