416eccf942b74b5a8764caafe9ffe20b6064dedf
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-08-30 08:44:03 +0700

    feat : is valid & tag.

--- reflect.go
+++ reflect.go
@@ -7,23 +7,52 @@ import (
 
 
 type Sample struct {
-	Name string
+	Name string `required:"true" max:"10"`
 }
 
 type Person struct {
-	Name, Address, Email string
+	Name string `required:"true" max:"10"`
+	Address string `required:"true" max:"10"`
+	Email string `required:"true" max:"10"`
 }
 
 func readField(value any) {
 	var valueType reflect.Type = reflect.TypeOf(value)
 	fmt.Println("Type Name", valueType.Name())
 	for i := 0; i < valueType.NumField(); i++ {
-		var valueField reflect.StructField = valueType.Field(i)
-		fmt.Printf(valueField.Name, " with type ", valueField.Type)
+		var structField reflect.StructField = valueType.Field(i)
+		fmt.Printf(structField.Name, " with type ", structField.Type)
+		fmt.Println(structField.Tag.Get("required"))
+		fmt.Println(structField.Tag.Get("max"))
 	}
 }
-func main(){
 
-	readField(Person{"Rama","Indonesia","anon@gmail.com"})
-	readField(Sample{"Fajar"})
+func IsValid(value any) bool {
+	result := true
+	t := reflect.TypeOf(value)
+	for i := 0; i < t.NumField(); i++ {	
+		f := t.Field(i)
+		if f.Tag.Get("required") == "true" {
+			data := reflect.ValueOf(value).Field(i).Interface()
+			result = data != ""
+
+			if result == false {
+				return result
+			}
+		}
+	}
+	return result
+}
+
+func main(){
+	// readField(Person{"Rama","Indonesia","anon@gmail.com"})
+	// readField(Sample{"Fajar"})
+
+	person := Person {
+		Name:"Fajar",
+		Address:"",
+		Email:"test@gmail.com",
+	}
+
+	fmt.Println(IsValid(person))
 }
